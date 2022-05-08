c88b9d8cc499c1d5f413414b9f0367950ba0b717
OmerRe <omercohen90@gmail.com>
2022-05-08 10:19:32 +0300

    add_bit_map

--- project/Code/backround_subtractor.py
+++ project/Code/backround_subtractor.py
@@ -5,10 +5,13 @@ import matplotlib.pyplot as plt
 from Code.utils import extract_video_parameters
 
 
-def subtruct_background(input_frames):
+def subtruct_background_by_median(input_frames):
     params = extract_video_parameters(input_frames)
     number_of_frames = input_video.get(cv2.CAP_PROP_FRAME_COUNT)
     selected_frames = np.arange(0, number_of_frames, int(number_of_frames / 5))
+    bgSubtractor = cv2.createBackgroundSubtractorMOG2()
+    # create the kernel that will be used to remove the noise in the foreground mask
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
     frames = []
     for frameOI in selected_frames:
@@ -31,19 +34,55 @@ def subtruct_background(input_frames):
         grayMedianFrame_with_noise = cv2.blur(grayMedianFrame, (5, 33), 0)
         frame_with_noise = cv2.blur(frame, (5, 33), 0)
         dframe = cv2.absdiff(frame_with_noise, grayMedianFrame_with_noise)
-        th, dframe = cv2.threshold(dframe, 50, 255, cv2.THRESH_BINARY)
-        dframe_hsv = cv2.cvtColor(dframe, cv2.COLOR_BGR2YUV)
-        mask = cv2.inRange(dframe_hsv, (120, 0, 0), (240, 190, 255))
-        bak = dframe.copy()
+        # th, dframe = cv2.threshold(dframe, 50, 255, cv2.THRESH_BINARY)
+        dframe = cv2.cvtColor(dframe, cv2.COLOR_BGR2GRAY)
 
-        # replace with black
-        bak[mask > 0] = (0, 0, 0)
+        # get the foreground mask
+        foregroundMask = bgSubtractor.apply(frame)
+
+        # remove some of noise
+        foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
+
+        # dframe_hsv = cv2.cvtColor(dframe, cv2.COLOR_BGR2YUV)
+        # mask = cv2.inRange(dframe_hsv, (120, 0, 0), (240, 190, 255))
+        # bak = dframe.copy()
+        #
+        # # replace with black
+        # bak[mask > 0] = (0, 0, 0)
         # Display image
-        cv2.imshow('frame', bak)
+        aggregated_frame = cv2.bitwise_and(dframe, foregroundMask)
+        # aggregated_frame = (aggregated_frame > 200).astype(np.uint8)
+        th, aggregated_frame = cv2.threshold(aggregated_frame, 37, 255, cv2.THRESH_BINARY)
+        # aggregated_frame = cv2.morphologyEx(aggregated_frame, cv2.MORPH_OPEN, kernel)
+        cv2.imshow('frame', aggregated_frame)
         cv2.waitKey(1)
 
+def subtruct_background_by_MOG2(input_frames):
+    # creation of a videoCapture object with the opening of a video file or a capture device
+    bgSubtractor = cv2.createBackgroundSubtractorMOG2()
+    # create the kernel that will be used to remove the noise in the foreground mask
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
+    while True:
+        # get the next picture
+        ret, frame = input_frames.read()
+
+        if ret:
+            # get the foreground mask
+            foregroundMask = bgSubtractor.apply(frame)
+
+            # remove some of noise
+            foregroundMask = cv2.morphologyEx(foregroundMask, cv2.MORPH_OPEN, kernel)
+
+            cv2.imshow('subtraction', foregroundMask)
+
+            if cv2.waitKey(30) == ord('e'):
+                break
+
+
 input_video = cv2.VideoCapture('../Outputs/stabilized_302828991_316524800.avi')
-subtruct_background(input_video)
+subtruct_background_by_median(input_video)
+# subtruct_background_by_MOG2(input_video)
 # Release video object
 input_video.release()
 
